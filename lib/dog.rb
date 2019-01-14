d1c9eb9c283fc337938a4cc89ef2ced10de24c6d
github username <github email address>
2019-01-13 21:27:00 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
   # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+@@all = []
+def self.all
+  @@all
+end
+
+  def initialize(name, mood = 'nervous' )
+    @name = name
+    @mood = mood
+    @@all << self
+  end
+
+
+
 end
